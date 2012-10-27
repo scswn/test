182133358556d1b699eef652758ce71a00ae9273
Wudi <wudicgi@gmail.com>
2012-10-27 20:36:36 +0800

    增加 Usage 提示信息 将输出名称修改为 volfluc.exe 将 AssemblyInfo.cs 中设置的版本号修改为 0.0.1.0

--- Program.cs
+++ Program.cs
@@ -8,6 +8,25 @@ namespace VolumeFluctuation
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Volume Fluctuation Index Calculator (v{0}).",
+                    System.Reflection.Assembly.GetEntryAssembly().GetName().Version.ToString());
+                Console.WriteLine("");
+                Console.WriteLine("Usage: volfluc [filename]");
+                Console.WriteLine("");
+                Console.WriteLine("Example: volfluc test.mp4");
+                Console.WriteLine("Output:  0.500");
+#if DEBUG
+                Console.ReadKey();
+#endif
+                Environment.Exit(0);
+            }
+
+#if DEBUG
+            Console.ReadKey();
+#endif
+            Environment.Exit(0);
         }
     }
 }
